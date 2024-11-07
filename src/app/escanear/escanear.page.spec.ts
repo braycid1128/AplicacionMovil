690ae2274cf0a53aa1373582cd208baade7c0691
CETECOM <cetecom_conce@duoc.cl>
2024-11-07 09:26:04 -0300

    Agregar escaner

--- src/app/escanear/escanear.page.spec.ts
+++ src/app/escanear/escanear.page.spec.ts
@@ -1,11 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EscanearPage } from './escanear.page';
+import { IonicModule } from '@ionic/angular';
 
 describe('EscanearPage', () => {
   let component: EscanearPage;
   let fixture: ComponentFixture<EscanearPage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [EscanearPage],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(EscanearPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
