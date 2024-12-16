b27c72d992bae068753d0c76b1e0f35d2050d3c7
Brayan <brayancid1128@gmail.com>
2024-12-16 12:55:42 -0300

    aa

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'brayan.appasistencia1',
   appName: 'appasistencia1',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    BarcodeScanner: {
+      // Opcionales: agrega configuraciones que necesites para el escaneo
+    },
+  },
 };
 
 export default config;
